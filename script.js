36444757817ed33dbadccb07674f715f9a61f32d
Natália Veras <natalia.veras.campos@avanade.com>
2021-09-26 22:28:06 +0200

    [bug] Winning logic compare right value

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ document.getElementById("dice").addEventListener("click", function () {
   const diceImg = document.getElementById("dice-img");
   diceImg.src = `images/dice-${randomNumber}.png`;
   console.log("roll dice", randomNumber);
-  //   If rolls "1" loses current points and skips turn
-  //   Add points to current
+  //   Add points to current player's score
   currentScores[currentPlayer].innerHTML = currentScore += randomNumber;
 
+  //   If rolls "1" loses current points and skips turn
   if (randomNumber === 1 && currentPlayer === 0) {
     currentPlayer = 1;
     currentScore = 0;
@@ -37,19 +37,38 @@ document.getElementById("dice").addEventListener("click", function () {
 
 // Hold points
 document.getElementById("hold").addEventListener("click", function () {
-  console.log("hold points");
-  points[currentPlayer].innerHTML =
-    Number(points[currentPlayer].innerHTML) + currentScore;
-  currentScore = 0;
-  currentScores[currentPlayer].innerHTML = currentScore;
-
-  if (currentPlayer === 0) {
-    currentPlayer = 1;
-    player[0].classList.remove("active-player");
-    player[1].classList.add("active-player");
+  if (Number(points[currentPlayer].innerHTML) >= 10) {
+    console.log(`Player ${currentPlayer + 1} wins!`);
   } else {
-    currentPlayer = 0;
-    player[1].classList.remove("active-player");
-    player[0].classList.add("active-player");
+    console.log("hold points");
+    points[currentPlayer].innerHTML =
+      Number(points[currentPlayer].innerHTML) + currentScore;
+    currentScore = 0;
+    currentScores[currentPlayer].innerHTML = currentScore;
+
+    // FIX
+    // sta controllando valore prima di arrivare a 10 e quindi vincitore in ritardo
+    console.log("player points", points[0].nodeName, points[0].innerHTML);
+    if (currentPlayer === 0) {
+      currentPlayer = 1;
+      player[0].classList.remove("active-player");
+      player[1].classList.add("active-player");
+    } else {
+      currentPlayer = 0;
+      player[1].classList.remove("active-player");
+      player[0].classList.add("active-player");
+    }
   }
+});
+
+// New game
+document.getElementById("restart").addEventListener("click", function () {
+  currentPlayer = 0;
+  currentScore = 0;
+  points[0].innerHTML = currentScore;
+  points[1].innerHTML = currentScore;
+  currentScores[0].innerHTML = currentScore;
+  currentScores[1].innerHTML = currentScore;
+  player[1].classList.remove("active-player");
+  player[0].classList.add("active-player");
 });
