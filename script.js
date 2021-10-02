79d2aa11dcd7950ff526574878cbbf89694636ba
Natália Veras <natalia.veras.campos@avanade.com>
2021-10-02 18:24:03 +0200

    Info banner with game rules, refactoring, minor UI improvements

--- script.js
+++ script.js
@@ -1,93 +1,114 @@
 "use script";
 
-// TODO
-/*
-Refactor
-*/
-
-let currentScore = 0;
-let currentPlayer = 0;
-const winningPoints = 20;
+let currentScore;
+let currentPlayer;
+let playing;
+const winningPoints = 100;
 const currentScores = document.querySelectorAll(".current-score");
 const player = document.querySelectorAll(".player");
 const playerTitle = document.querySelectorAll(".player-title");
 const points = document.querySelectorAll(".points");
-const banner = document.getElementById("banner");
-banner.innerHTML = `Hit ${winningPoints} points to win! <span class="dismiss-banner">Dismiss</span>`;
 const diceImg = document.getElementById("dice-img");
+const banner = document.getElementById("banner");
+banner.innerHTML = `
+<h2>Game rules 😃</h2>
+<ul>
+<li>
+   Roll the dice to accumulate points.
+  </li>
+  <li>
+    Click on hold to add current points to your score.
+  </li>
+  <li>
+    Rolling a 1 skips your turn and you lose the accumulated points. 
+  </li>
+  <li>
+   First player to add ${winningPoints} points to their score wins.
+  </li>
+  <li class="developed">
+    Developed with <i class="fas fa-heart"></i> 
+  </li>
+</ul>
+<span class="dismiss-banner">Dismiss</span>
+`;
+const dismissBanner = document.querySelector(".dismiss-banner");
+const showInfo = document.getElementById("info");
+init();
 
-// Dismiss banner
-document
-  .querySelector(".dismiss-banner")
-  .addEventListener("click", function () {
-    banner.classList.add("hide");
-  });
+// Starting game conditions
+function init() {
+  currentPlayer = 0;
+  currentScore = 0;
+  playing = true;
+  playerTitle[0].innerHTML = `Player 1`;
+  playerTitle[1].innerHTML = `Player 2`;
+  player[0].classList.remove("winner");
+  player[1].classList.remove("winner");
+  points[0].innerHTML = currentScore;
+  points[1].innerHTML = currentScore;
+  currentScores[0].innerHTML = currentScore;
+  currentScores[1].innerHTML = currentScore;
+  diceImg.classList.add("hide");
+  player[0].classList.add("active-player");
+  player[1].classList.remove("active-player");
+}
+
+// Show info
+showInfo.addEventListener("click", function () {
+  console.log("show info");
+  banner.classList.toggle("hide");
+});
+
+// Dismiss info banner
+dismissBanner.addEventListener("click", function () {
+  banner.classList.add("hide");
+});
 
 // Roll dice
 document.getElementById("dice").addEventListener("click", function () {
-  //   Generate a random number from 1 to 6
-  const randomNumber = Math.trunc(Math.random() * 6) + 1;
-  //   Show dice face image
-  diceImg.src = `images/dice-${randomNumber}.png`;
-  diceImg.classList.remove("hide");
-  console.log("rolled dice", randomNumber);
-  //   Add points to current player's score
-  currentScores[currentPlayer].innerHTML = currentScore += randomNumber;
-  //   If rolls "1" loses current points and skips turn
-  if (randomNumber === 1) {
-    currentScore = 0;
-    player[currentPlayer].classList.remove("active-player");
-    currentScores[currentPlayer].innerHTML = 0;
-    if (currentPlayer === 0) {
-      currentPlayer = 1;
-      player[1].classList.add("active-player");
-    } else {
-      currentPlayer = 0;
-      player[0].classList.add("active-player");
+  if (playing) {
+    //   Generate a random number from 1 to 6
+    const randomNumber = Math.trunc(Math.random() * 6) + 1;
+    //   Show dice face image
+    diceImg.src = `images/dice-${randomNumber}.png`;
+    diceImg.classList.remove("hide");
+    //   Add points to current player's score
+    currentScores[currentPlayer].innerHTML = currentScore += randomNumber;
+    //   If rolls "1" loses current points and skips turn
+    if (randomNumber === 1) {
+      currentScore = 0;
+      currentScores[currentPlayer].innerHTML = 0;
+      switchPlayers();
     }
   }
 });
 
 // Hold points
 document.getElementById("hold").addEventListener("click", function () {
-  points[currentPlayer].innerHTML =
-    Number(points[currentPlayer].innerHTML) + currentScore;
-  currentScore = 0;
-  currentScores[currentPlayer].innerHTML = currentScore;
-
-  if (Number(points[currentPlayer].innerHTML) >= winningPoints) {
-    player[currentPlayer].classList.add("winner");
-    playerTitle[currentPlayer].innerHTML = `Player ${
-      currentPlayer + 1
-    } wins! 🎉`;
+  if (playing) {
+    points[currentPlayer].innerHTML =
+      Number(points[currentPlayer].innerHTML) + currentScore;
+    currentScore = 0;
+    currentScores[currentPlayer].innerHTML = 0;
     // Game is over
-    document.getElementById("dice").disabled = true;
-    document.getElementById("hold").disabled = true;
-  } else {
-    player[currentPlayer].classList.remove("active-player");
-    if (currentPlayer === 0) {
-      currentPlayer = 1;
-      player[1].classList.add("active-player");
+    if (Number(points[currentPlayer].innerHTML) >= winningPoints) {
+      playing = false;
+      player[currentPlayer].classList.add("winner");
+      playerTitle[currentPlayer].innerHTML = `Player ${
+        currentPlayer + 1
+      } wins! 🎉`;
     } else {
-      currentPlayer = 0;
-      player[0].classList.add("active-player");
+      switchPlayers();
     }
   }
 });
 
-// New game
-document.getElementById("restart").addEventListener("click", function () {
-  player[currentPlayer].classList.remove("winner");
-  playerTitle[currentPlayer].innerHTML = `Player ${currentPlayer + 1}`;
-  currentPlayer = 0;
-  currentScore = 0;
-  points[0].innerHTML = currentScore;
-  points[1].innerHTML = currentScore;
-  currentScores[0].innerHTML = currentScore;
-  currentScores[1].innerHTML = currentScore;
-  player[1].classList.remove("active-player");
-  player[0].classList.add("active-player");
-  diceImg.classList.add("hide");
-  document.getElementById("dice").disabled = false;
-  document.getElementById("hold").disabled = false;
-});
+// Switch Players
+const switchPlayers = function () {
+  player[currentPlayer].classList.remove("active-player");
+  currentPlayer = currentPlayer === 0 ? 1 : 0;
+  player[currentPlayer].classList.add("active-player");
+};
+
+// New game button
+document.getElementById("restart").addEventListener("click", init);
